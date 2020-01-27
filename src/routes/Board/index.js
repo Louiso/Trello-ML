fb97aa13685b79a87d9b188a1a2148bd764ea855
Louiso <Gransullca.25@gmail.com>
2020-01-26 19:35:44 -0500

    [update] se elimino hook

--- src/routes/Board/index.js
+++ src/routes/Board/index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import { BOARD_FRAGMENT } from '../../apollo/Board/fragments'
-import { useReadFragment } from '../../hooks/useReadFragment'
 
 export default () => {
   const { boardId } = useParams()
-  const { loading, data } = useReadFragment({ id: `Board:${boardId}`, fragment: BOARD_FRAGMENT })
-  console.log("TCL: loading, data", loading, data)
   // const { loading, data } = useQuery(GET_BOARD_BY_ID, { variables: { boardId } })
   // console.log("TCL: loading, data", loading, data)
   return (
